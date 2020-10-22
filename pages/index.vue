2a3c82ac35d686a1068fb39e229fdce3bf9c5759
xujizhong <1013633243@qq.com>
2020-10-22 15:34:49 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div>
+            
+            <h1 class="title">
+                nuxt 
+                {{ip}}
+            </h1>
+            <div class="links">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    async asyncData({ $axios }) {
+        const ip = await $axios.$get('http://localhost:3000/api/users/login');
+        console.log(ip)
+        return { ip };
+    }
+};
+</script>
+
